bf05b6552e03cb3494dabb2e07c8635995f5a0f5
Casper Sidor <casper.sidor@gmail.com>
2023-03-08 12:26:58 +0100

    message added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Hello from './components/Hello'
+import Message from './components/Message'
 
 function App() {
   return (
     <div className="App">
+      <Message />
       {/* <Greet></Greet>
       <Welcome></Welcome> 
       <Hello></Hello>*/}
-      <Greet name="Cathrine" nickname="Cat"/>
+      {/* <Greet name="Cathrine" nickname="Cat"/>
       <Greet name="Casper" nickname="Sidor"/>
-      <Greet name="Christer" nickname="Basse"/>
+      <Greet name="Christer" nickname="Basse"/> */}
     </div>
     
   );
