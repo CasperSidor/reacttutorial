4403bf1968d6297c58885431f47fa8100a74bc0a
CasperSidor <93120638+CasperSidor@users.noreply.github.com>
2023-03-22 09:48:29 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,34 +8,15 @@ import Message from './components/Message'
 function App() {
   return (
     <div className="App">
-      Søgefeldt <input />
-      <hr />
-      <label>
-        Kategorier:
-        <p/ >
-        Tegneserier <input type="checkbox" name="CartoonCheckbox"/>
-        Litteratur <input type="checkbox" name="LitteratureCheckbox"/>
-        Roman <input type="checkbox" name="NovelCheckbox"/>
-      </label>
-      <hr />
-
-      Sorter efter:
-        <p/ >
-        <label>
-          <input type="radio" name="myRadio" value="Price" />
-          Pris
-        </label>
-        <label>
-          <input type="radio" name="myRadio" value="Year" />
-          Årstal
-        </label>
-
-      {/* <Greet></Greet>
+    
+      <Greet></Greet>
+      <p/ >
       <Welcome></Welcome> 
-      <Hello></Hello>*/
-      /* <Greet name="Cathrine" nickname="Cat"/>
+      <p/ >
+      <Hello></Hello>
+      <Greet name="Cathrine" nickname="Cat"/>
       <Greet name="Casper" nickname="Sidor"/>
-      <Greet name="Christer" nickname="Basse"/> */}
+      <Greet name="Christer" nickname="Basse"/> 
     </div>
     
   );
